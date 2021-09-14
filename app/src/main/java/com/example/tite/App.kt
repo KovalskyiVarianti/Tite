6bfd070b5174916e040e53402c796959f620c23e
Oleh Kovalskyi <kovalskyioleg1243@gmail.com>
2021-09-14 17:12:04 +0300

    added basic dependencies

--- app/src/main/java/com/example/tite/App.kt
+++ app/src/main/java/com/example/tite/App.kt
@@ -1,6 +1,23 @@
 package com.example.tite
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
+import timber.log.Timber
 
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        initKoin()
+    }
+
+    private fun initKoin() {
+        GlobalContext.startKoin {
+            androidLogger()
+            androidContext(this@App)
+            //TODO modules(appModule)
+        }
+    }
 }
